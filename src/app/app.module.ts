29591649a273220c013ee10e39ecd5141ed2e7d4
LUISA FERNANDA LEON <Luisaleon34563@gmail.com>
2025-02-17 17:07:57 -0500

    angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TopbarComponent } from './topbar/topbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    TopbarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CommonModule
+  ],
+  providers: [],
+  exports:[],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
